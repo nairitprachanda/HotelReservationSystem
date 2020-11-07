15c6613365fea17d915c9492fb9911ef00828ed5
nairitprachanda <nairitprachanda@gmail.com>
2020-11-07 16:21:58 +0530

    [nairit] Refactor:Adding Hotel name in the Reservation System

---
+++ HotelReservationSystem/HotelReservationSystem/Hotel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservationSystem
+{
+    public class Hotel
+    {
+        public string name;
+        public int ratesForRegularCustomer;
+        public Hotel()
+        {
+            name = "";
+            ratesForRegularCustomer = 0;
+        }
+        public Hotel(string name, int ratesForRegularCustomer)
+        {
+            this.name = name;
+            this.ratesForRegularCustomer = ratesForRegularCustomer;
+        }
+    }
+}
---
+++ HotelReservationSystem/HotelReservationSystem/HotelSystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservationSystem
+{
+    public class HotelSystem
+    {
+        public List<Hotel> hotelList;
+        public HotelSystem()
+        {
+            hotelList = new List<Hotel>();
+        }
+        public void AddHotel(Hotel hotel)
+        {
+            hotelList.Add(hotel);
+        }
+    }
+}
