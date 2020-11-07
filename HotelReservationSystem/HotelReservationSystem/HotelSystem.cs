33b2d732e33ff4b4ef9272bc1f1ba6c109ffed0e
nairitprachanda <nairitprachanda@gmail.com>
2020-11-07 23:22:39 +0530

    [nairit} Refactor: Finding the cheapest hotel with good ratings

--- HotelReservationSystem/HotelReservationSystem/HotelSystem.cs
+++ HotelReservationSystem/HotelReservationSystem/HotelSystem.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
+using System.Linq;
 namespace HotelReservationSystem
 {
     public class HotelSystem
     {
+
         public List<Hotel> hotelList;
         CheckDate checkDate = new CheckDate();
         public HotelSystem()
@@ -45,6 +47,31 @@ namespace HotelReservationSystem
                 }
             }
             return cheapestHotel;
+        }
+        public Hotel GetCheapestHotelWithBestRating(DateTime[] dates)
+        {
+            double noOfWeekend = 0;
+            double cheapestPrice = Double.MaxValue;
+            double noOfWeekday = 0;
+            List<Hotel> cheapestHotels = new List<Hotel>();
+            foreach (var date in dates)
+            {
+                if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+                    noOfWeekend++;
+            }
+            TimeSpan timeSpan = dates[1].Subtract(dates[0]);
+            noOfWeekday = timeSpan.TotalDays - noOfWeekend;
+            foreach (var hotel in hotelList)
+            {
+                double priceDuringStay = hotel.weekdayRatesForRegularCustomer * noOfWeekday + hotel.weekendRatesForRegularCustomer * noOfWeekend;
+                if (priceDuringStay < cheapestPrice)
+                {
+                    cheapestPrice = priceDuringStay;
+                    cheapestHotels.Add(hotel);
+                }
+            }
+            List<Hotel> sortedCheapestHotelsAsPerRating = cheapestHotels.OrderByDescending(x => x.rating).ToList();
+            return sortedCheapestHotelsAsPerRating[0];
         }
     }
 }
--- HotelReservationSystem/UnitTestProject1/UnitTest1.cs
+++ HotelReservationSystem/UnitTestProject1/UnitTest1.cs
@@ -64,5 +64,19 @@ namespace HotelReservationSystemTestProject
             hotelSystem.AddHotel(new Hotel("Ridgewood", 5, 220, 150));
             Assert.AreEqual(3, hotelSystem.hotelList[0].rating);
         }
+        //6
+        [TestMethod]
+        public void GivenWeekendAndWeekdayRateReturnCheapestHotelAlongWithRating()
+        {
+            hotelSystem.AddHotel(new Hotel("Lakewood", 3, 110, 90));
+            hotelSystem.AddHotel(new Hotel("Bridgewood", 4, 150, 50));
+            hotelSystem.AddHotel(new Hotel("Ridgewood", 5, 220, 150));
+            string[] dates = "11Sep2020,12Sep2020".Split(",");
+            DateTime[] date = new DateTime[2];
+            date[0] = DateTime.Parse(dates[0]);
+            date[1] = DateTime.Parse(dates[1]);
+            Hotel cheapestHotel = hotelSystem.GetCheapestHotelWithBestRating(date);
+            Assert.AreEqual(4, cheapestHotel.rating);
+        }
     }
 }
